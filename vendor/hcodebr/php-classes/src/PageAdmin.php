8855224fc3f3945356b2511b54f81132924492ff
Renan Teixeira <renanpugas@gmail.com>
2018-07-24 13:54:27 -0300

    criandmos padgeadmin e tpl admin

---
+++ vendor/hcodebr/php-classes/src/PageAdmin.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Hcode;
+
+class PageAdmin extends Page {
+
+	public function __construct($opts = array(), $tpl_dir = "/CursoCompletoPHP7/ecommerce/views/admin/") {
+
+		//chama o construtor da classe pai
+		parent::__construct($opts, $tpl_dir);
+
+	}
+
+}
+
+?>
